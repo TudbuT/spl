75a54d3dfd405a91fc4632af3766dc7cf8257112
TudbuT <tudbut@tudbut.de>
2023-03-06 04:28:51 +0100

    replace http with net:http, add net module

--- src/runtime.rs
+++ src/runtime.rs
@@ -833,7 +833,9 @@ impl Type {
             q.append(&mut VecDeque::from(t.lock_ro().parents.clone()));
         }
         for property in to_apply.into_iter().rev() {
-            object.property_map.insert(property, Value::Null.spl());
+            if !object.property_map.contains_key(&property) {
+                object.property_map.insert(property, Value::Null.spl());
+            }
         }
     }
 
