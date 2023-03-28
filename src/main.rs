908dc4b06b9d1678e65ff3cb35ee00ff1c7a55bf
TudbuT <tudbut@tudbut.de>
2023-03-28 02:54:16 +0200

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use spl::start_file;
+use spl::{start_file, find_in_splpath};
 
 use std::env::args;
 
 fn main() {
-    if let Err(x) = start_file(&args().nth(1).expect("no file provided to be started.")) {
+    if let Err(x) = start_file(&args().nth(1).unwrap_or_else(|| find_in_splpath("repl.spl").expect("no file to be run"))) {
         println!("{x:?}");
     }
 }
